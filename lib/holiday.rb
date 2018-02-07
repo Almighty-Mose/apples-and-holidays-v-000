88686b60484018e14e541f1e8a38b9c9b7d71ba3
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-07 00:55:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |attribute, data|
     puts "#{season.to_s.capitalize}:"
-    puts "  #{holiday.to_s.capitalize}: #{data}"
+    puts "  #{attribute.to_s.capitalize}: #{data}"
     end
   end
 end
