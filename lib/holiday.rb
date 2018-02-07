3c603c7fc48fdd0f4037b4fbcaa6712170e95b9f
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-07 00:41:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season, holiday|
     holiday.each do |attribute, data|
-      data.collect.flatten
+      data.collect
     end
   end
 end
