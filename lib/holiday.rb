7eafa597357fcf63fc3940893215b4503c5276ee
Branden Mosley <branden.mosley1337@gmail.com>
2018-02-07 00:35:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday_hash[:winter][holiday] << supply
+      holiday.each do |attribute, data|
+        data << supply
       end
     end
 end
